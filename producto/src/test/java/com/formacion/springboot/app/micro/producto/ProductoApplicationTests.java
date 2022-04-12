b660bf25ec89180c8b53220cde8e6f3b75ed78e5
sirchema <chema.flo@gmail.com>
2022-04-12 17:57:46 +0200

    Add files via upload

---
+++ producto/src/test/java/com/formacion/springboot/app/micro/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.springboot.app.micro.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
